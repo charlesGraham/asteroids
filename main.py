a0a1b0662df15e8a3c3107a90b8b5aae88df296d
Charles <15911636+charlesGraham@users.noreply.github.com>
2025-06-04 14:55:01 -0700

    add asteroid and asteroidfield classes

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    # grouping sprites
-    updateable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    player.containers = (updateable, drawable)
-
-    # add player to the drawable and updateable groups
-    drawable.add(player)
-    updateable.add(player)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-            screen.fill("black")
+        updatable.update(dt)
 
-            for item in drawable:
-                item.draw(screen)
+        screen.fill("black")
 
-            updateable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
 
-            pygame.display.flip()
-            dt = clock.tick(60) / 1000
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
