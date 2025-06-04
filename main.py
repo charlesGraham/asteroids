2258975e06240304ac2a5e3f989f01ba0666873f
Charles <15911636+charlesGraham@users.noreply.github.com>
2025-06-04 12:29:49 -0700

    use groups to manage game objects

--- main.py
+++ main.py
@@ -11,14 +11,27 @@ def main():
     dt = 0.0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # grouping sprites
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.containers = (updateable, drawable)
+
+    # add player to the drawable and updateable groups
+    drawable.add(player)
+    updateable.add(player)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
             screen.fill("black")
-            player.draw(screen)
-            player.update(dt)
+
+            for item in drawable:
+                item.draw(screen)
+
+            updateable.update(dt)
+
             pygame.display.flip()
             dt = clock.tick(60) / 1000
 
