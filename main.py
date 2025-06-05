d40a2519fefc155ff41c4d2873be77be85215702
Charles <15911636+charlesGraham@users.noreply.github.com>
2025-06-04 17:15:15 -0700

    add asteroid splitting

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
             for shot in shots:
                 if asteroid.detect_collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
